1d2ac6c2640c7ef20a3bb4eaf728ee5308abebe1
qqvm <54307539+qqvm@users.noreply.github.com>
2024-08-27 09:41:41 +0200

    Add files via upload

---
+++ sync.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    let box = document.getElementsByClassName('box')[0];
+    let navEl = document.createElement('nav');
+    navEl.className = 'navbar';
+    let navList = '';
+    
+
+    let containers = Array.from(document.getElementsByClassName('item'));
+    let prefix = 'goecharts_';
+    let groups = [];
+    for (i = 0; i < containers.length; i++) {
+        if (i > 1){
+            let group = containers[i].id.split('v')[0];
+            if (!groups.includes(group)){
+                groups.push(group);
+                navList += '<li><a href="#' + containers[i].id + '">' + group + '</a></li>\n'
+            }
+            eval(prefix + containers[i].id + '.group = "' + group + '";');
+        }
+    }
+    groups.forEach(g => echarts.connect(g));
+
+    navEl.innerHTML = `
+    <ul>
+      <li><a class="active" href="#ALLvParallel">ALL</a></li>
+      `+ navList +`
+    </ul>
+    `.trim();
+    box.insertBefore(navEl, box.firstChild);
+
+    document.querySelector('.navbar').addEventListener('click', (e) => {
+        if (e.target.tagName.toLowerCase() === 'a') {
+          document.querySelectorAll('.navbar a').forEach(e => e.classList.remove('active'));
+          e.target.classList.add('active');
+      }
+  });
+});
